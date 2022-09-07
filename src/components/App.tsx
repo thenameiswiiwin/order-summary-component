b1c1a32f10f5366d069cf4b9ac56632ab0e95ffa
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-09-06 21:08:54 -0500

    styled

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,39 @@
+import Hero from '../assets/images/illustration-hero.svg'
+import Music from '../assets/images/icon-music.svg'
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <main className="flex h-screen items-center justify-center bg-paleBlue bg-mobile bg-contain bg-no-repeat font-redHat sm:bg-desktop sm:bg-auto sm:bg-top">
+      <div className="mx-5 max-w-[375px] space-y-9 overflow-hidden rounded-3xl bg-white">
+        <img src={Hero} alt="hero" />
+        <section className="mx-7 flex flex-col gap-5 pb-7 text-center">
+          <h2 className="text-2xl font-bold">Order Summary</h2>
+          <p className="px-3 text-sm font-medium leading-relaxed text-gray-400">
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+          <div className="flex items-center justify-around rounded-xl bg-veryPaleBlue px-2 py-4">
+            <img src={Music} alt="music logo" />
+            <span className="pr-8 text-sm leading-relaxed">
+              <h4 className="font-bold">Annual Plan</h4>
+              <p className="font-medium text-gray-400">$59.99/year</p>
+            </span>
+            <a
+              className="text-sm font-bold text-brightBlue underline hover:no-underline hover:opacity-75"
+              href="#"
+            >
+              Change
+            </a>
+          </div>
+          <button className="rounded-xl bg-brightBlue py-3.5 font-medium text-white shadow-xl shadow-paleBlue hover:opacity-75">
+            Proceed to Payment
+          </button>
+          <button className="font-bold text-gray-500 hover:text-black">
+            Cancel Order
+          </button>
+        </section>
+      </div>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        redHat: ['Red Hat Display', 'sans-serif']
+      },
+      colors: {
+        paleBlue: 'hsl(225, 100%, 94%)',
+        brightBlue: 'hsl(245, 75%, 52%)',
+        brightBlueShadow: 'hsl(245, 75%, 88%)',
+        veryPaleBlue: 'hsl(225, 100%, 98%)',
+        desaturatedBlue: 'hsl(224, 23%, 55%)',
+        darkBlue: 'hsl(223, 47%, 23%)'
+      },
+      backgroundImage: {
+        mobile: "url('./src/assets/images/pattern-background-mobile.svg')",
+        desktop: "url('./src/assets/images/pattern-background-desktop.svg')"
+      }
+    }
   },
   plugins: []
 }
